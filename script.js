da233cf652fbc5252d2eaa890b6dcd85410352c2
AbhishekSharma931 <116187016+AbhishekSharma931@users.noreply.github.com>
2024-12-18 14:54:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function togglePasswordVisibility(id) {
+function togglePasswordVisibility(id) { 
     const field = document.getElementById(id);
     field.type = field.type === "password" ? "text" : "password";
 }
